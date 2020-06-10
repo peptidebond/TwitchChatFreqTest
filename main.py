da2647bbed1a00a0ee4126a7ecedc0b039ecb6f7
David Morris <d3morris@gmail.com>
2020-06-09 20:56:59 -0400

    made my analysis more readable

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ mirth = "|".join([r'(\bl+o+l+\b)',r'\bl+m+a+o+\b',r'(\bro+f+l+\b)',r'(\b(he)+\b)
 
 count = 0
 df = []
+#--------
+#This part loads chat data from all the chat files
+#--------
 for filename in os.listdir('chat_data'):
     count += 1
     timelist = []
@@ -35,39 +38,43 @@ for filename in os.listdir('chat_data'):
         data = {'Video': vidlist, 'Name': namelist, 'Time': timelist, 'Message': msglist}
         df += [pd.DataFrame(data)]
 
+#--------
+#This part does the anlysis
+#--------
 first = True
 
 for i in range(len(df)-1):
+    #Start off by grouping everything into buckets of 10 seconds
     grouped_time = df[i].groupby(pd.Grouper(key='Time',freq='10s'))
     size = grouped_time.size()
+
+    #Calculate a rolling sum of our buckets (this smooths out the data. i dont like my data chunky
     rollingsum = size.rolling('60s').sum()
-    n=15
+
+    #This is how far apart each hilight has to be
+    #In this case n=15 means "150 seconds" apart (10s buckets * 15)
+    n = 15
 
     rollingsum = rollingsum.reset_index()
+
+    #This part identifies high-points using the rolling sums
     extremes = argrelextrema(rollingsum[0].values, np.greater_equal, order=n)
     rollingsum['maxes'] = rollingsum.iloc[extremes[0]][0]
     rollingsum['maxes'] = rollingsum['maxes']*100//rollingsum['maxes'].max()
     rollingsum['vidID'] = df[i].iloc[0]['Video']
+
+    #I'm subtracting one minute here because the chat laughter detected usually comes *after* the joke
+    #Also im subtracting a minute because of the width of the rolling sum but thats whatever
     t = (rollingsum['Time'] - pd.Timedelta(minutes=1)).dt
+
+    #Chunk up our datetimes into nicer packets
     rollingsum['h'] = t.strftime('%H')
     rollingsum['m'] = t.strftime('%M')
     rollingsum['s'] = t.strftime('%S')
+
+    #Roll it all up into one dataframe
     if first:
         rollingsums = rollingsum[rollingsum.maxes.notna()]
         first = False
     else:
         rollingsums = rollingsums.append(rollingsum[rollingsum.maxes.notna()])
-
-'''
-plt.scatter(df.index, df['maxes'], c='r')
-plt.plot(df.index, df[0])
-
-prevbiggest=-9999
-for index in extremes[0]:
-    if (index-prevbiggest > 60):
-        prevbiggest = index
-        plt.annotate(df.iloc[index]['Time'].__format__('%H:%M:%S'), xy=(index, df.iloc[index][0]), xytext=(index, df.iloc[index][0]+5),
-            arrowprops=dict(facecolor='black', shrink=0.05),
-            )
-
-plt.show()'''
